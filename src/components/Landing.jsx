e32f84bab1eb880b903566ec2d2ee85c260f8968
Ravi Bansal <bansalravi011@gmail.com>
2025-08-15 14:14:05 +0530

    Team update yet again

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -6,7 +6,7 @@ export default function Landing() {
 
         <>
 
-            <div className="bg-image h-screen w-screen grid place-items-center overflow-x-hidden">
+            <div className="h-screen w-screen grid place-items-center overflow-x-hidden">
 
                 <h1 className="heading lg:text-10xl md:text-7xl sm:text-5xl text-6xl text-center justify-center  font-extrabold tracking-normal leading-loose">T U R I N G <br></br> S A P I E N S</h1>
 
